f51315b47413a1bc06057bce054d5ad92fc302e9
lasbe <devlasbe@gmail.com>
2024-11-13 16:03:51 +0900

    feat: payload 타입 변경

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ErrorResponseInterceptor } from './error-response/error-response.interceptor';
+import { SuccessResponseInterceptor } from './success-response/success-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ErrorResponseInterceptor());
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/success-response/success-response.interceptor.ts
+++ src/success-response/success-response.interceptor.ts
@@ -15,9 +15,11 @@ export class SuccessResponseInterceptor<T>
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<{ payload: T }> {
-    const handler = context.getHandler().name;
+    console.log(context.getArgs());
+    const request = context.switchToHttp().getRequest()?.url;
+
     return next.handle().pipe(
-      map((data) => ({ handler, payload: data })), // 응답 데이터를 payload 형태로 변환
+      map((data) => ({ request, payload: data })), // 응답 데이터를 payload 형태로 변환
     );
   }
 }
