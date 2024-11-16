ba83d1d1e2ff2c875509b436389b540a35cccc98
lasbe <devlasbe@gmail.com>
2024-11-16 22:34:53 +0900

    chore: swagger default url 변경

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('franchise');
 
   app.useGlobalInterceptors(new ErrorResponseInterceptor());
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
