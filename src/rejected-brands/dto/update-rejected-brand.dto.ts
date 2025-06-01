b059528f129ac00f5b6da95ad05f48fb7d2f5322
lasbe <devlasbe@gmail.com>
2025-06-01 16:57:07 +0900

    chore: remove rejected-brands

--- src/rejected-brands/dto/update-rejected-brand.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRejectedBrandDto } from './create-rejected-brand.dto';
-
-export class UpdateRejectedBrandDto extends PartialType(CreateRejectedBrandDto) {}
--- src/rejected-brands/rejected-brands.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RejectedBrandsService } from './rejected-brands.service';
-import { RejectedBrandsController } from './rejected-brands.controller';
-
-@Module({
-  controllers: [RejectedBrandsController],
-  providers: [RejectedBrandsService],
-})
-export class RejectedBrandsModule {}
