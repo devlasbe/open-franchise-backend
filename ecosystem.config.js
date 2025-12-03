56b12f10f8ce92f0b736a900af1d467a99a7af4d
lasbe <devlasbe@gmail.com>
2025-12-03 21:55:22 +0900

    chore: refactor Dockerfile and ecosystem.config.js to use environment variables directly and update deployment commands

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,18 @@ module.exports = {
       name: 'franchise-backend',
       script: './dist/src/main.js',
       exec_mode: 'cluster',
-      instances: '2', // CPU 코어 수
-      max_memory_restart: '1000M', // 프로세스의 메모리가 1000MB에 도달하면 reload 실행
-      merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합침
-      autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
+      instances: '2',
+      max_memory_restart: '1000M',
+      merge_logs: true,
+      autorestart: true,
       env_production: {
         NODE_ENV: 'production',
         PORT: 3000,
-        DATABASE_URL: 'VAL_DATABASE_URL',
-        OPENAPI_KEY: 'VAL_OPENAPI_KEY',
-        DEFAULT_YEAR: 'VAL_DEFAULT_YEAR',
-        JWT_SECRET: 'VAL_JWT_SECRET',
+        // [변경] 문자열 대신 실제 환경변수를 받아옵니다.
+        DATABASE_URL: process.env.DATABASE_URL,
+        OPENAPI_KEY: process.env.OPENAPI_KEY,
+        DEFAULT_YEAR: process.env.DEFAULT_YEAR,
+        JWT_SECRET: process.env.JWT_SECRET,
       },
     },
   ],
